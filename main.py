8f1c0df3907b96d1662a16817dde10e7c0c9649f
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-03-08 21:43:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 #http://localhost:3000
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://kenny-bot.vercel.app/"}})
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 
 openai.api_key =  os.environ.get('OPEN_AI_API_KEY')
